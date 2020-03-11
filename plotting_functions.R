3ecb9c300625a7ca12cd204729bc30f93f88e76d
longmuiryan <rlongmuir@ucsb.edu>
2020-03-11 12:42:06 -0400

    commit

--- plotting_functions.R
+++ plotting_functions.R
@@ -3,7 +3,7 @@
 # functional programming w/ the tidyverse 
 # ========================================================================
 
-setwd("~/Desktop/Git/edwinbet")
+# setwd("~/Desktop/Git/edwinbet")
 
 # ------------------------------------------------------------------------
 # packages 
@@ -12,7 +12,7 @@ setwd("~/Desktop/Git/edwinbet")
 library(tidyverse)
 
 # ------------------------------------------------------------------------
-# plotting function 
+# plotting functions
 # ------------------------------------------------------------------------
 
 wine_qq <- function(dataframe, variable, title = NULL){
@@ -20,15 +20,17 @@ wine_qq <- function(dataframe, variable, title = NULL){
   p1 <- ggplot(dataframe, aes(sample = !! variable))  +
     stat_qq(distribution = stats::qnorm) +
     stat_qq_line(distribution = stats::qnorm) +
-    labs(title = paste(title, "Box Plot") ) 
+    labs(title = paste(title, "QQ Plot") ) + 
+    theme_minimal() + theme(text = element_text(size = 14))
   return(p1)
 }
 
-wine_box <- function(dataframe, variable, title = NULL){
+wine_box <- function(dataframe, variable, title = NULL, ...){
   variable <- enquo(variable)
   p1 <- ggplot(dataframe, aes(x = " ", y = !! variable)) +
-    geom_boxplot() + 
-    labs(title = paste(title, "Box Plot"), y = "")
+    geom_boxplot(...) + 
+    labs(title = paste(title, "Box Plot"), y = "", x = "") + 
+    theme_minimal()  + theme(text = element_text(size = 14))
   return(p1)
 }
 
@@ -36,7 +38,8 @@ wine_hist <- function(dataframe, variable, title = NULL, ...){
   variable <- enquo(variable)
   p1 <- ggplot(dataframe, aes(x = !! variable)) +
     geom_histogram(...) +  
-    labs(title = paste(title, "Box Plot"), y = "Frequency", x = "")
+    labs(title = paste(title, "Histogram"), y = "Frequency", x = "") +
+    theme_minimal()  + theme(text = element_text(size = 14))
   return(p1)
 }
 
@@ -44,8 +47,29 @@ wine_hist <- function(dataframe, variable, title = NULL, ...){
 # test plotting function 
 # ------------------------------------------------------------------------
 
-#  wine <- read.csv("data/winemag-data-130k-v2.csv")
+# wine <- read.csv("data/winemag-data-130k-v2.csv")
 #  wine_box(wine, price)
 #  wine_hist(wine, points, bins = 20)
 
+# ------------------------------------------------------------------------
+# remove outliers function 
+# ------------------------------------------------------------------------
 
+filter_outliers <- function(dataframe, variable){
+  variable <- enquo(variable)
+  series <- select(dataframe, !! variable) %>% unlist()
+  IQR <- IQR(series, na.rm = T)
+  Q <- quantile(series, probs=c(.25, .75), na.rm = T)
+  LB <- Q[1] - 1.5*IQR
+  UB <- Q[2] + 1.5*IQR
+  cat(" Upper bound:", UB, "\n Lower bound:", LB)
+  return(filter(dataframe, !! variable > LB & !! variable 
+    < UB & !is.na(!! variable)))
+}
+
+# ------------------------------------------------------------------------
+# test outliers function 
+# ------------------------------------------------------------------------
+
+# wine <- read.csv("data/winemag-data-130k-v2.csv")
+# wine2 <- filter_outliers(wine, price)
